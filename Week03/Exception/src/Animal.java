9ed96dca4695c30f58bc507c4a17452e8cfc7d35
ReeseHatfield <hatfield.69@wright.edu>
2025-05-27 15:14:12 -0400

    in class code may 27th

--- Week03/Exception/src/Animal.java
+++ Week03/Exception/src/Animal.java
@@ -1,8 +1,10 @@
 public abstract class Animal implements MakeNoise {
+    // behaves the same 
     public void walk(int distance){
         System.out.println("This animal is walking " + distance + "meters");
     }
 
 
+    // this behaves differently based on who the child is
     public abstract void playFetch();
 }
